b6979679079eab3db5e752cee45bd502ce961274
alphameo <pavel.2014.play@gmail.com>
2025-12-14 13:23:37 +0300

    feat(app): add demo view
    fix(app): org imports

--- app/src/main/java/com/github/alphameo/App.java
+++ app/src/main/java/com/github/alphameo/App.java
@@ -3,12 +3,54 @@
  */
 package com.github.alphameo;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalUnit;
+import java.util.List;
 
+import com.github.alphameo.university.AdministrativePerson;
+import com.github.alphameo.university.Course;
+import com.github.alphameo.university.Faculty;
+import com.github.alphameo.university.Institute;
+import com.github.alphameo.university.Lecturer;
+import com.github.alphameo.university.Project;
+import com.github.alphameo.university.ProjectParticipation;
+import com.github.alphameo.university.ResearchPerson;
+import com.github.alphameo.university.University;
+
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        var vsu = new University();
+
+        var mathPhysicaEq = new Course("1a", "Equations of Math and Physics", 4);
+
+        var krylovetsky = new Lecturer("Krylovetsky", "111", "kryl@mail.ru",
+                "Extremal Algorythms");
+        krylovetsky.addCourse(mathPhysicaEq);
+
+        var fedutinov = new ResearchPerson("Fedutinov", "222", "fed@mai.ru", "idk");
+        var nuzhnykh = new ResearchPerson("Nuzhnykh", "000", "nuzhnykh@mail.ru", "GoodPain)");
+        var admin = new AdministrativePerson("Adminsky", "adm-01", "vsu-admin@mail.ru");
+        vsu.hireEmployee(krylovetsky);
+        vsu.hireEmployee(fedutinov);
+        vsu.hireEmployee(nuzhnykh);
+        vsu.hireEmployee(admin);
+
+        var csFaculty = new Faculty("CS", krylovetsky);
+        var ammFaculty = new Faculty("amm", fedutinov);
+        vsu.addFaculty(csFaculty);
+        vsu.addFaculty(ammFaculty);
+
+        var piit = new Institute("Programming and Information Technologies", "Universitetskaya square, 1, cab. 381a");
+        var ct = new Institute("Digital Technologies", "Universitetskaya square, 1, cab. ---");
+        csFaculty.addInstitute(piit);
+        csFaculty.addInstitute(ct);
+        krylovetsky.assignToInstitute(ct);
+        fedutinov.assignToInstitute(piit);
+
+        var goodEdu = new Project("GoodEducation", LocalDateTime.now(),
+                LocalDateTime.now().plusDays(12));
+        var projectParticipations = List.of(new ProjectParticipation(12, goodEdu, nuzhnykh));
+
+        System.out.println(vsu);
     }
 }
